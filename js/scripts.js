03d0724bd8d8ea05c3dab9414456de3a5847fd2f
Micaela Jawor <micaelajawor@yahoo.com>
2019-01-18 14:10:18 -0800

    try to make color picker affect button color

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,34 @@
 $(document).ready(function() {
+
+  $(function() {
+	//bg color selector
+  $(".color").click(function(){
+		var color = $(this).attr("data-value");
+    $("body").css("background-color", color);
+	});
+
+  //add color picker if supported
+  if (Modernizr.inputtypes.color) {
+    $(".picker").css("display", 'inline-block');
+    var c = document.getElementById('colorpicker');
+    c.addEventListener('change', function(e) {
+      //d.innerHTML = c.value;
+      var color = c.value;
+      $("body").css("background-color", color);
+		}, false);
+  }
+});
+function pickColor() {
+  $("#colorpicker").click();
+}
+
+
+
+  $('#color').form-control ({
+    onChange: function(hsb, hex, rgb) {
+      $("#btn").css("color", '#' + hex);
+    }
+  });
   $("form#quiz").submit(function(event) {
     var name = $("#your-name").val();
 
