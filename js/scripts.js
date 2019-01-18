ad66c748801d62aef7483979f9a46625fb54eb41
Micaela Jawor <micaelajawor@yahoo.com>
2019-01-18 12:56:00 -0800

    work more on js scripts quiz is starting to work

--- js/scripts.js
+++ js/scripts.js
@@ -8,21 +8,22 @@ $(document).ready(function() {
     var size = $("input:radio[name=size]:checked").val();
     var development = $("input:radio[name=development]:checked").val();
     var answer = $("input:radio[name=answer]:checked").val();
+    var scripts = $("input:radio[name=scripts]:checked").val();
 
     if (answer !== 'yes') {
       alert("Select yes when you are ready");
     }
-    
-    else if (size === 'large' && development === 'back' && answer === 'yes' && build === 'internal') {
-      $("#language").after('C#' + '<br><img src="img/c#.png" alt="The C# icon">');
+
+    else if (size === 'large' && development !== 'front' && build === 'database' && scripts === 'newer') {
+      $("#language").after("C#" + "<br><img src='img/csharp.png' alt='The C# icon'>");
     }
-    else if (size === 'large' && development === 'back' && answer === 'yes' && build === 'apps') {
+    else if (size === 'large' && development !== 'front' && build === 'web' && scripts === 'newer') {
       $("#language").after("Java" + "<br><img src='img/java.png' alt='The Java icon'>");
     }
-    else if (size === 'small' && development === 'back' && answer === 'yes' && build === 'websites') {
+    else if (size === 'small' && development !== 'front' && build === 'web' && scripts === 'newer') {
       $("#language").after("PHP" + "<br><img src='img/php.png' alt='The PHP icon'>");
     }
-    else if (size === 'small' && development === 'back' && answer === 'no' && build === 'web-apps') {
+    else if (development !== 'front' && scripts === 'older') {
       $("#language").after("Ruby" + "<br><img src='img/ruby.png' alt='The Ruby icon'>");
     }
     else {
