a1c5c4baf5b150ed672f682e5a1b6c70afdbfa1e
Micaela Jawor <micaelajawor@yahoo.com>
2019-01-18 09:56:13 -0800

    work on js scripts

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,27 @@
-var favoriteColor = $("#color").val();
+$(document).ready(function() {
+  $("form#quiz").submit(function(event) {
+    var name = $("#your-name").val();
+
+    var favoriteColor = $("#color").val();
+
+    var build = $("input:radio[name=build]:checked").val();
+    var size = $("input:radio[name=size]:checked").val();
+    var development = $("input:radio[name=development]:checked").val();
+    var answer = $("input:radio[name=answer]:checked").val();
+
+    if (answer === "yes"){
+      $("#show").toggle(function() {
+        $("p").show();
+    })
+    else {
+      $("hide").toggle(function() {
+        $("p").hide();
+      })
+    }
+
+      })
+
+      alert("Please enter your name");
+  });
+
+});
