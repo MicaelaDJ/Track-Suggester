2fc0c7f6b50a2f7f266a3cfc3bd7ee82b932cc38
Micaela Jawor <micaelajawor@yahoo.com>
2019-01-18 16:19:30 -0800

    start adding show hide elements to quiz

--- js/scripts.js
+++ js/scripts.js
@@ -31,20 +31,24 @@ $(document).ready(function() {
       alert("Select yes when you are ready");
     }
     else if (size === 'large' && development !== 'front' && build === 'database' && scripts === 'newer') {
-      $("#language").after("C#" + "<br><img src='img/csharp.png' alt='The C# icon'>");
-      $("#quiz").hide();
+      $("#language").after("C#");
+      $("#csharp").show();
+      $("#css").hide();
+      $("#java").hide();
+      $("#php").hide();
+      $("#ruby").hide();
     }
     else if (size === 'large' && development !== 'front' && build === 'web' && scripts === 'newer') {
-      $("#language").after("Java" + "<br><img src='img/java.png' alt='The Java icon'>");
+      $("#language").after("Java");
     }
     else if (size === 'small' && development !== 'front' && build === 'web' && scripts === 'newer') {
-      $("#language").after("PHP" + "<br><img src='img/php.png' alt='The PHP icon'>");
+      $("#language").after("PHP");
     }
     else if (development !== 'front' && scripts === 'older') {
-      $("#language").after("Ruby" + "<br><img src='img/ruby.png' alt='The Ruby icon'>");
+      $("#language").after("Ruby");
     }
     else {
-      $("#language").after("CSS" + "<br><img src='img/css.png' alt='The CSS icon'>");
+      $("#language").after("CSS");
     }
     event.preventDefault();
   });
